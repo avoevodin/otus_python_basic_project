dd24f894d3489bcc3423b49e669b93f3ca2cfeae
avo888 <avoevodin8888@gmail.com>
2022-08-23 18:27:42 +0300

    added product model updated requirements.txt with pillow

--- myshop/products/models.py
+++ myshop/products/models.py
@@ -4,12 +4,34 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, db_index=True)
     slug = models.SlugField(max_length=128, db_index=True, unique=True)
-    description = models.CharField(max_length=512)
+    description = models.CharField(max_length=512, blank=True)
 
     class Meta:
         ordering = ("name",)
         verbose_name = "Category"
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, db_index=True)
+    slug = models.SlugField(max_length=128, db_index=True)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.PROTECT
+    )
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+    description = models.CharField(max_length=512, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("name",)
+        index_together = (("id", "slug"),)
 
     def __str__(self):
         return self.name
