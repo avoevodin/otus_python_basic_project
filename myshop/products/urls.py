49d8a8341fdd466752fb6b3ede40e64dd6bd1094
avo888 <avoevodin8888@gmail.com>
2022-08-26 15:46:28 +0300

    fixed typos

--- myshop/products/urls.py
+++ myshop/products/urls.py
@@ -1,18 +1,18 @@
-from django.urls import re_path
+from django.urls import path
 
 from .views import ProductListView, ProductDetailView
 
 app_name = "products"
 
 urlpatterns = [
-    re_path(r"^$", ProductListView.as_view(), name="products_list"),
-    re_path(
-        r"^(?P<category_slug>[-\w]+)/$",
+    path("", ProductListView.as_view(), name="products_list"),
+    path(
+        "<slug:category_slug>",
         ProductListView.as_view(),
         name="products_list_by_category",
     ),
-    re_path(
-        r"^(?P<pk>\d+)/(?P<slug>[-\w]+)$",
+    path(
+        "<int:pk>/<slug:slug>",
         ProductDetailView.as_view(),
         name="product_detail",
     ),
