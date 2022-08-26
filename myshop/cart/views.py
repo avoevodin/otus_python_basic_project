4ee1c2b154d288610edf272e41f61d90a84134e5
avo888 <avoevodin8888@gmail.com>
2022-08-26 15:39:23 +0300

    added cart add, remove, detail views added cart add form

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from products.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+def get_request_cart_data(request, product_pk):
+    """
+    TODO
+    """
+    cart = Cart(request)
+    product = get_object_or_404(Product, pk=product_pk)
+    return cart, product
+
+
+@require_POST
+def cart_add(request, product_pk):
+    """
+    TODO
+    """
+    cart, product = get_request_cart_data(request, product_pk)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            product=product,
+            quantity=cd["quantity"],
+            update_quantity=cd["update"],
+        )
+
+    return redirect("cart:cart_detail")
+
+
+@require_POST
+def cart_remove(request, product_pk):
+    """
+    TODO
+    """
+    cart, product = get_request_cart_data(request, product_pk)
+    cart.remove(product)
+    return redirect("cart:cart_detail")
+
+
+def cart_detail(request):
+    """
+    TODO
+    """
+    cart = Cart(request)
+    return render(request, "cart/cart_detail.html", {"cart": cart})
--- myshop/products/views.py
+++ myshop/products/views.py
@@ -1,5 +1,4 @@
-from django.conf import settings
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views import generic
 
 from .models import Product, Category
