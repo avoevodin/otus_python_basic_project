ebebea6b5d60584116e3af68433f9a1719d12835
avo888 <avoevodin8888@gmail.com>
2022-08-28 22:56:53 +0300

    added category model tests

--- myshop/products/tests/test_models/test_product.py
+++ myshop/products/tests/test_models/test_product.py
@@ -1,7 +1,37 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from ..factories.product import ProductFactory
+from ..factories.product import ProductFactory, CategoryFactory
+
+
+class CategoryModelTestCase(TestCase):
+    """
+    TODO
+    """
+
+    def setUp(self):
+        """
+        TODO
+        """
+        self.category = CategoryFactory()
+
+    def test_product_str(self):
+        """
+        TODO
+        """
+        self.assertEqual(str(self.category), self.category.name)
+
+    def test_product_abs_url(self):
+        """
+        TODO
+        """
+        self.assertEqual(
+            self.category.get_absolute_url(),
+            reverse(
+                "products:products_list_by_category",
+                args=[self.category.slug],
+            ),
+        )
 
 
 class ProductModelTestCase(TestCase):
