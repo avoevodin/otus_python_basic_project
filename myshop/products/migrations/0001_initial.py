f7a7fb1b888ec8f5d393a5bfd7d9a4d69e6376ae
avo888 <avoevodin8888@gmail.com>
2022-08-29 05:22:32 +0300

    fixed typos with black

--- myshop/products/migrations/0001_initial.py
+++ myshop/products/migrations/0001_initial.py
@@ -8,42 +8,64 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(db_index=True, max_length=128)),
-                ('slug', models.SlugField(max_length=128, unique=True)),
-                ('description', models.CharField(blank=True, max_length=512)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(db_index=True, max_length=128)),
+                ("slug", models.SlugField(max_length=128, unique=True)),
+                ("description", models.CharField(blank=True, max_length=512)),
             ],
             options={
-                'verbose_name': 'Category',
-                'verbose_name_plural': 'Categories',
-                'ordering': ('name',),
+                "verbose_name": "Category",
+                "verbose_name_plural": "Categories",
+                "ordering": ("name",),
             },
         ),
         migrations.CreateModel(
-            name='Product',
+            name="Product",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(db_index=True, max_length=128)),
-                ('slug', models.SlugField(max_length=128)),
-                ('image', models.ImageField(blank=True, upload_to='products/%Y/%m/%d')),
-                ('description', models.CharField(blank=True, max_length=512)),
-                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('stock', models.PositiveIntegerField()),
-                ('available', models.BooleanField(default=True)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('category', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='products', to='products.category')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(db_index=True, max_length=128)),
+                ("slug", models.SlugField(max_length=128)),
+                ("image", models.ImageField(blank=True, upload_to="products/%Y/%m/%d")),
+                ("description", models.CharField(blank=True, max_length=512)),
+                ("price", models.DecimalField(decimal_places=2, max_digits=10)),
+                ("stock", models.PositiveIntegerField()),
+                ("available", models.BooleanField(default=True)),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("updated", models.DateTimeField(auto_now=True)),
+                (
+                    "category",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="products",
+                        to="products.category",
+                    ),
+                ),
             ],
             options={
-                'ordering': ('name',),
-                'index_together': {('id', 'slug')},
+                "ordering": ("name",),
+                "index_together": {("id", "slug")},
             },
         ),
     ]
